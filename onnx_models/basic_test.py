8e82c2da912613d54879a59c2404183196f96857
Scott Todd <scott.todd0@gmail.com>
2024-09-06 11:44:27 -0700

    Compile with iree-compile.

--- onnx_models/basic_test.py
+++ onnx_models/basic_test.py
@@ -10,6 +10,7 @@ import pytest
 import subprocess
 import urllib.request
 from pathlib import Path
+from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -57,6 +58,29 @@ def import_onnx_model(onnx_path: Path):
     return imported_mlir_path
 
 
+def compile_model(input_program: Path, config_name: str, compile_flags: List[str]):
+    cwd = THIS_DIR
+    compiled_module_path = input_program.with_name(
+        input_program.stem + f"_{config_name}.vmfb"
+    )
+    compile_args = ["iree-compile", input_program.relative_to(cwd)]
+    compile_args.extend(compile_flags)
+    compile_args.extend(["-o", compiled_module_path.relative_to(cwd)])
+    compile_cmd = subprocess.list2cmdline(compile_args)
+    logging.getLogger().info(
+        f"Launching compile command:\n"  #
+        f"  cd {cwd} && {compile_cmd}"
+    )
+    ret = subprocess.run(compile_cmd, shell=True, capture_output=True, cwd=cwd)
+    if ret.returncode != 0:
+        logging.getLogger().error(f"Compilation of '{compiled_module_path}' failed")
+        logger.error(ret.stdout.decode("utf-8"))
+        logger.error("iree-import-onnx stderr:")
+        logger.error(ret.stderr.decode("utf-8"))
+        raise RuntimeError(f"  '{compiled_module_path.name}' compile failed")
+    return compiled_module_path
+
+
 def test_basic():
     print("test_basic")
 
@@ -67,8 +91,10 @@ def test_basic():
 
     upgraded_path = upgrade_onnx_model(original_path)
     imported_mlir_path = import_onnx_model(upgraded_path)
+    compiled_module_path = compile_model(
+        imported_mlir_path, "cpu", ["--iree-hal-target-backends=llvm-cpu"]
+    )
     # TODO(scotttodd): Load input data
-    # TODO(scotttodd): Compile with IREE
     # TODO(scotttodd): Run with IREE
     # TODO(scotttodd): Load into ONNX Runtime
     # TODO(scotttodd): Run with ONNX Runtime
