e0726d40f71fe79b45a302ab17af66b8bad00c41
Scott Todd <scott.todd0@gmail.com>
2024-09-06 12:09:38 -0700

    Generate random test input data and run the compiled program.

--- onnx_models/basic_test.py
+++ onnx_models/basic_test.py
@@ -5,7 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import logging
+import numpy as np
 import onnx
+import struct
 import pytest
 import subprocess
 import urllib.request
@@ -13,6 +15,7 @@ from pathlib import Path
 from typing import List
 
 logger = logging.getLogger(__name__)
+rng = np.random.default_rng(0)
 
 THIS_DIR = Path(__file__).parent
 ARTIFACTS_DIR = THIS_DIR / "artifacts"
@@ -21,7 +24,7 @@ ONNX_CONVERTER_OUTPUT_MIN_VERSION = 17
 
 
 # TODO(#18289): use real frontend API, import model in-memory?
-def upgrade_onnx_model(original_path: Path):
+def upgrade_onnx_model_version(original_path: Path):
     original_model = onnx.load_model(original_path)
     converted_model = onnx.version_converter.convert_version(
         original_model, ONNX_CONVERTER_OUTPUT_MIN_VERSION
@@ -37,7 +40,7 @@ def upgrade_onnx_model(original_path: Path):
 
 
 # TODO(#18289): use real frontend API, import model in-memory?
-def import_onnx_model(onnx_path: Path):
+def import_onnx_model_to_mlir(onnx_path: Path):
     imported_mlir_path = onnx_path.with_suffix(".mlir")
     logging.info(
         f"Importing '{onnx_path.relative_to(THIS_DIR)}' to '{imported_mlir_path.relative_to(THIS_DIR)}'"
@@ -50,7 +53,8 @@ def import_onnx_model(onnx_path: Path):
     ]
     ret = subprocess.run(exec_args, capture_output=True)
     if ret.returncode != 0:
-        logger.error(f"Import of '{onnx_path.name}' failed!\niree-import-onnx stdout:")
+        logger.error(f"Import of '{onnx_path.name}' failed!")
+        logger.error("iree-import-onnx stdout:")
         logger.error(ret.stdout.decode("utf-8"))
         logger.error("iree-import-onnx stderr:")
         logger.error(ret.stderr.decode("utf-8"))
@@ -58,44 +62,115 @@ def import_onnx_model(onnx_path: Path):
     return imported_mlir_path
 
 
-def compile_model(input_program: Path, config_name: str, compile_flags: List[str]):
+def compile_mlir_with_iree(mlir_path: Path, config_name: str, compile_flags: List[str]):
     cwd = THIS_DIR
-    compiled_module_path = input_program.with_name(
-        input_program.stem + f"_{config_name}.vmfb"
-    )
-    compile_args = ["iree-compile", input_program.relative_to(cwd)]
+    iree_module_path = mlir_path.with_name(mlir_path.stem + f"_{config_name}.vmfb")
+    compile_args = ["iree-compile", mlir_path.relative_to(cwd)]
     compile_args.extend(compile_flags)
-    compile_args.extend(["-o", compiled_module_path.relative_to(cwd)])
+    compile_args.extend(["-o", iree_module_path.relative_to(cwd)])
     compile_cmd = subprocess.list2cmdline(compile_args)
-    logging.getLogger().info(
+    logger.info(
         f"Launching compile command:\n"  #
         f"  cd {cwd} && {compile_cmd}"
     )
     ret = subprocess.run(compile_cmd, shell=True, capture_output=True, cwd=cwd)
     if ret.returncode != 0:
-        logging.getLogger().error(f"Compilation of '{compiled_module_path}' failed")
+        logger.error(f"Compilation of '{iree_module_path}' failed")
+        logger.error("iree-compile stdout:")
         logger.error(ret.stdout.decode("utf-8"))
-        logger.error("iree-import-onnx stderr:")
+        logger.error("iree-compile stderr:")
         logger.error(ret.stderr.decode("utf-8"))
-        raise RuntimeError(f"  '{compiled_module_path.name}' compile failed")
-    return compiled_module_path
+        raise RuntimeError(f"  '{iree_module_path.name}' compile failed")
+    return iree_module_path
+
+
+def run_iree_module(iree_module_path: Path, run_flags: List[str]):
+    cwd = THIS_DIR
+    run_args = ["iree-run-module", f"--module={iree_module_path.relative_to(cwd)}"]
+    run_args.extend(run_flags)
+    run_cmd = subprocess.list2cmdline(run_args)
+    logger.info(
+        f"Launching run command:\n"  #
+        f"  cd {cwd} && {run_cmd}"
+    )
+    ret = subprocess.run(run_cmd, shell=True, capture_output=True, cwd=cwd)
+    if ret.returncode != 0:
+        logger.error(f"Run of '{iree_module_path}' failed")
+        logger.error("iree-run-module stdout:")
+        logger.error(ret.stdout.decode("utf-8"))
+        logger.error("iree-run-module stderr:")
+        logger.error(ret.stderr.decode("utf-8"))
+        raise RuntimeError(f"  '{iree_module_path.name}' run failed")
+    logger.info(f"Run of '{iree_module_path}' succeeded")
+    logger.info("iree-run-module stdout:")
+    logger.info(ret.stdout.decode("utf-8"))
+    logger.info("iree-run-module stderr:")
+    logger.info(ret.stderr.decode("utf-8"))
+
+
+# map numpy dtype -> (iree dtype, struct.pack format str)
+numpy_to_iree_dtype_map = {
+    np.dtype("int64"): ("si64", "q"),
+    np.dtype("uint64"): ("ui64", "Q"),
+    np.dtype("int32"): ("si32", "i"),
+    np.dtype("uint32"): ("ui32", "I"),
+    np.dtype("int16"): ("si16", "h"),
+    np.dtype("uint16"): ("ui16", "H"),
+    np.dtype("int8"): ("si8", "b"),
+    np.dtype("uint8"): ("ui8", "B"),
+    np.dtype("float64"): ("f64", "d"),
+    np.dtype("float32"): ("f32", "f"),
+    np.dtype("float16"): ("f16", "e"),
+    np.dtype("bool"): ("i1", "?"),
+}
+
+
+def pack_ndarray_to_binary(ndarr: np.ndarray):
+    mylist = ndarr.flatten().tolist()
+    dtype = ndarr.dtype
+    bytearr = b""
+    if dtype in numpy_to_iree_dtype_map:
+        iree_dtype = numpy_to_iree_dtype_map[dtype][1]
+        bytearr = struct.pack(f"{len(mylist)}{iree_dtype}", *mylist)
+    else:
+        raise NotImplementedError(
+            f"Unsupported data type in pack_ndarray_to_binary(): '{dtype}'"
+        )
+    return bytearr
+
+
+def write_binary_to_file(ndarr: np.ndarray, filename: Path):
+    with open(filename, "wb") as f:
+        bytearr = pack_ndarray_to_binary(ndarr)
+        f.write(bytearr)
 
 
 def test_basic():
-    print("test_basic")
+    if not ARTIFACTS_DIR.is_dir():
+        ARTIFACTS_DIR.mkdir(parents=True)
 
     # TODO(scotttodd): move to fixture with cache / download on demand
     onnx_url = "https://github.com/onnx/models/raw/main/validated/vision/classification/mobilenet/model/mobilenetv2-12.onnx"
     original_path = ARTIFACTS_DIR / "mobilenetv2-12.onnx"
     # urllib.request.urlretrieve(onnx_url, original_path)
 
-    upgraded_path = upgrade_onnx_model(original_path)
-    imported_mlir_path = import_onnx_model(upgraded_path)
-    compiled_module_path = compile_model(
+    upgraded_path = upgrade_onnx_model_version(original_path)
+    imported_mlir_path = import_onnx_model_to_mlir(upgraded_path)
+    iree_module_path = compile_mlir_with_iree(
         imported_mlir_path, "cpu", ["--iree-hal-target-backends=llvm-cpu"]
     )
-    # TODO(scotttodd): Load input data
-    # TODO(scotttodd): Run with IREE
+
+    # TODO(scotttodd): prepare_input helper function
+    random_data = rng.random((1, 3, 224, 224), dtype=np.float32)
+    random_data_path = original_path.with_name(original_path.stem + "_input_0.bin")
+    write_binary_to_file(random_data, random_data_path)
+    # logger.info(dummy_data)
+
+    run_iree_module(
+        iree_module_path,
+        ["--device=local-task", f"--input=1x3x224x224xf32=@{random_data_path}"],
+    )
+
     # TODO(scotttodd): Load into ONNX Runtime
     # TODO(scotttodd): Run with ONNX Runtime
     # TODO(scotttodd): Compare results
