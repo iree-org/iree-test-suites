7250953380b96fa7ac22b1cb947d4653806bf5f7
Scott Todd <scott.todd0@gmail.com>
2024-11-21 14:34:45 -0800

    Import tests for the other validated/vision/ folders.

---
+++ onnx_models/tests/model_zoo/validated/vision/body_analysis_models_test.py
@@ -0,0 +1,64 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# https://github.com/onnx/models/tree/main/validated/vision/body_analysis/
+
+import pytest
+
+from .....utils import *
+
+artifacts_subdir = "model_zoo/validated/vision/body_analysis"
+
+
+def test_age_gender_age_googlenet(compare_between_iree_and_onnxruntime):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/age_gender/models/age_googlenet.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
+
+
+def test_age_gender_gender_googlenet(compare_between_iree_and_onnxruntime):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/age_gender/models/gender_googlenet.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
+
+
+@pytest.mark.size_medium
+def test_age_gender_vgg_ilsvrc_16_age_imdb_wiki(compare_between_iree_and_onnxruntime):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/age_gender/models/vgg_ilsvrc_16_age_imdb_wiki.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
+
+
+@pytest.mark.size_medium
+def test_age_gender_vgg_ilsvrc_16_gender_imdb_wiki(
+    compare_between_iree_and_onnxruntime,
+):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/age_gender/model/vgg_ilsvrc_16_gender_imdb_wiki.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
+
+
+def test_emotion_ferplus(
+    compare_between_iree_and_onnxruntime,
+):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/emotion_ferplus/model/emotion-ferplus-8.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
+
+
+@pytest.mark.skip("ONNXRuntimeError")
+def test_ultraface(
+    compare_between_iree_and_onnxruntime,
+):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/body_analysis/ultraface/models/version-RFB-320.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
---
+++ onnx_models/tests/model_zoo/validated/vision/style_transfer_models_test.py
@@ -0,0 +1,21 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# https://github.com/onnx/models/tree/main/validated/vision/style_transfer/
+
+import pytest
+
+from .....utils import *
+
+artifacts_subdir = "model_zoo/validated/vision/style_transfer"
+
+
+@pytest.mark.xfail(raises=IreeCompileException)
+def test_fast_neural_style(compare_between_iree_and_onnxruntime):
+    compare_between_iree_and_onnxruntime(
+        model_url="https://github.com/onnx/models/raw/main/validated/vision/style_transfer/fast_neural_style/model/mosaic-9.onnx",
+        artifacts_subdir=artifacts_subdir,
+    )
