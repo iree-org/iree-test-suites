75ae8259ab7e8efb24e5c721ffd1b3847bc0095e
Min <geomin12@amd.com>
2025-02-21 15:59:44 -0800

    Adding improved job summary details
    Signed-off-by: Min <geomin12@amd.com>

--- sharktank_models/benchmarks/run_benchmarks.py
+++ sharktank_models/benchmarks/run_benchmarks.py
@@ -9,7 +9,8 @@ import os
 from pathlib import Path
 import argparse
 import sys
-
+import tabulate
+import json
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,20 +31,36 @@ def main():
 
     THIS_DIR = Path(__file__).parent
 
-    with open("job_summary.md", "a") as job_summary:
-        print(
-            f"{sku.upper()} {model.upper()} Complete Benchmark Summary:\n\n",
-            file=job_summary,
-        )
-
     command = [
         "pytest",
         THIS_DIR / "test_model_benchmark.py",
         "--log-cli-level=info",
         "--timeout=600",
-        "--retries=7",
+        "--retries=0",
     ]
+    
+    with open("job_summary.md", "a") as job_summary, open("job_summary.json", "w+") as content:
+        print(f"{sku.upper()} {model.upper()} Complete Benchmark Summary:\n", file=job_summary)
+        json.dump({}, content)
+    
     ret_value = subprocess.run(command)
+    
+    markdown_data = {
+        "time_summary": ["Submodel name", "Current time (ms)", "Expected/golden time (ms)"],
+        "dispatch_summary": ["Submodel name", "Current dispatch count", "Expected/golden dispatch count"],
+        "size_summary": ["Submodel name", "Current binary size (bytes)", "Expected/golden binary size (bytes)"]
+    }
+    
+    with open("job_summary.md", "a") as job_summary, open("job_summary.json", "r") as content:
+        summary_data = json.loads(content.read())
+        for key, value in markdown_data.items():
+            if key in summary_data:
+                print("\n" + f"golden {key}" + "\n")
+                table_data = tabulate.tabulate(
+                    summary_data.get(key), headers=value, tablefmt="pipe"
+                )
+                print("\n" + table_data, file=job_summary)
+            
     return ret_value.returncode
 
 
