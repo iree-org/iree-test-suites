1a70aa8a7d685e5f7424993ef9bee820a4f6c3aa
geomin12 <geomin12@amd.com>
2025-03-04 13:57:37 -0800

    lint :)

--- sharktank_models/benchmarks/conftest.py
+++ sharktank_models/benchmarks/conftest.py
@@ -49,11 +49,14 @@ def pytest_sessionstart(session):
 
     # Keeping track of all external test files and their paths
     session.config.external_test_files = {}
-    path_of_external_test_files = Path(session.config.getoption("external_file_directory"))
+    path_of_external_test_files = Path(
+        session.config.getoption("external_file_directory")
+    )
     external_files = sorted(path_of_external_test_files.glob("*"))
     for external_file in external_files:
         file_name = external_file.name
         session.config.external_test_files[file_name] = external_file
+
 
 def pytest_sessionfinish(session, exitstatus):
     markdown_data = {
