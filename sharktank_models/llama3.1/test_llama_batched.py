8d52a6cea96623c4bfbc1944f862985820f3017c
geomin12 <geomin12@amd.com>
2025-03-26 16:38:57 -0700

    rebase and also fixing rebase

--- sharktank_models/llama3.1/test_llama_batched.py
+++ sharktank_models/llama3.1/test_llama_batched.py
@@ -16,18 +16,19 @@ block_size = 32
 page_size = kv_size * block_size
 
 THIS_DIR = pathlib.Path(__file__).parent
+ASSET_PATH = os.getenv("ASSET_PATH", default=str(THIS_DIR)) + "/llama3.1"
 
-BS1_DIR = THIS_DIR / "assets/bs1"
-BS4_DIR = THIS_DIR / "assets/bs4"
-BS32_DIR = THIS_DIR / "assets/bs32"
+BS1_DIR =  f"{ASSET_PATH}/assets/bs1"
+BS4_DIR = f"{ASSET_PATH}/assets/bs4"
+BS32_DIR = f"{ASSET_PATH}/assets/bs32"
 
-llama_mlir_bs1 = str(BS1_DIR / "toy_llama_bs1.mlir")
-llama_mlir_bs4 = str(BS4_DIR / "toy_llama_bs4.mlir")
-llama_mlir_bs32 = str(BS32_DIR / "toy_llama_bs32.mlir")
+llama_mlir_bs1 = f"{BS1_DIR}/toy_llama_bs1.mlir"
+llama_mlir_bs4 = f"{BS4_DIR}/toy_llama_bs4.mlir"
+llama_mlir_bs32 = f"{BS32_DIR}/toy_llama_bs32.mlir"
 
 # irpa files.
 # this is a list because sharding would have multiple irpa files
-llama_irpa = [str(THIS_DIR / "assets/toy_llama.irpa")]
+llama_irpa = [f"{ASSET_PATH}/assets/toy_llama.irpa"]
 
 
 class ToyLlama:
